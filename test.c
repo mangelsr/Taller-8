482b1dd35dc2d22424e3a2ab62a05553efb6a28e
Miguel Sanchez <mangelsr@live.com>
2017-06-22 16:38:44 -0500

    Taller terminado

--- test.c
+++ test.c
@@ -8,6 +8,7 @@
 #include "arduino-serial-lib.h"
 
 float calculateSD(float data[]);
+float calculateProm(float data[]);
 
 void error(char* msg)
 {
@@ -21,6 +22,9 @@ int main(int argc, char *argv[])
 	int baudrate = 9600;  // default
 
 	fd = serialport_init("/dev/ttyACM0", baudrate);
+	
+	float temperatura[12]={0};
+	float humedad[12]={0};
 
 	if( fd==-1 )
 	{
@@ -28,7 +32,38 @@ int main(int argc, char *argv[])
 		return -1;
 	}
 
-	
+	char com1 = 't';
+	char com2 = 'h';
+	int c = 0;
+	while (c<12){
+		int temp=0;
+
+		write(fd,&com1,1);
+		read(fd,&temp,1);
+		temperatura[c]=(float)temp;
+		printf("Temperatura: %d\n",temp);
+		usleep(500000);
+		
+		c++;	
+	}
+	printf("Prom = %.2f\n",calculateProm(temperatura));
+	printf("SD = %.2f\n\n",calculateSD(temperatura));
+
+	usleep(2000000);	
+	c=0;
+
+	while (c<12){
+		int hum=0;
+
+		write(fd,&com2,1);
+		read(fd,&hum,1);
+		humedad[c]=(float)hum;
+		printf("Humedad: %d\n",hum);
+		usleep(500000);	
+		c++;	
+	}
+	printf("Prom = %.2f\n",calculateProm(humedad));
+	printf("SD = %.2f\n",calculateSD(temperatura));	
 	close( fd );
 	return 0;	
 }
@@ -39,17 +74,37 @@ float calculateSD(float data[])
     float sum = 0.0, mean, standardDeviation = 0.0;
 
     int i;
-
-    for(i = 0; i < 10; ++i)
+	int cero=0;
+    for(i = 0; i < 12; ++i)
     {
-        sum += data[i];
+        if(data[i] != 0)		
+		        sum += data[i];
+		else
+			cero++;
     }
 
-    mean = sum/10;
+    mean = sum/(12-cero);
 
-    for(i = 0; i < 10; ++i)
-        standardDeviation += pow(data[i] - mean, 2);
-
-    return sqrt(standardDeviation / 10);
+    for(i = 0; i < 12; ++i){
+	if(data[i] != 0)
+	        standardDeviation += pow(data[i] - mean, 2);
+	}
+    return sqrt(standardDeviation / (12-cero));
 }
 
+float calculateProm(float data[])
+{
+	int cero=0;
+	int sum =0;
+	for(int i = 0; i < 12; ++i)
+	{
+		if(data[i] != 0)		
+		        sum += data[i];
+		else
+			cero++;
+	
+    	}
+
+    	float mean = sum/(12-cero);
+	return mean;
+}
